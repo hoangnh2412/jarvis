459b6d3025c3a256d244c53500dc83d7f8ccb219
Hoàng Nguyễn <hoangnh2412@gmail.com>
2020-07-16 16:58:32 +0700

    Add MinioService

---
+++ Infrastructures/Infrastructure.File.Minio/MinioOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Infrastructure.File.Minio
+{
+    public class MinioOptions
+    {
+        public string Endpoint { get; set; }
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string Region { get; set; }
+        public string SessionToken { get; set; }
+        public string BucketName { get; set; }
+    }
+}
