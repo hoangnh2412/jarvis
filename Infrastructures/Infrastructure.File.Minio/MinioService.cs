518b47f7ccdb03701274ef477f02022699feb131
ThuVu <thuvu240596@gmail.com>
2020-07-28 17:45:45 +0700

    #1587 remove field applicationinfo

--- Infrastructures/Infrastructure.File.Minio/MinioService.cs
+++ Infrastructures/Infrastructure.File.Minio/MinioService.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reactive.Linq;
-using System.Security.Cryptography;
-using System.Threading;
 using System.Threading.Tasks;
 using Infrastructure.File.Abstractions;
 using Microsoft.Extensions.Logging;
