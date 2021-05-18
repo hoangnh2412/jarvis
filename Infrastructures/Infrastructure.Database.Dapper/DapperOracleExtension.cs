bf300260a81bd0f2422bbe18f5ec3e7675dbc0f8
ThuVu <thuvu240596@gmail.com>
2021-05-18 17:45:54 +0700

    add dapperOracleExtension

---
+++ Infrastructures/Infrastructure.Database.Dapper/DapperOracleExtension.cs
@@ -0,0 +1,52 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Einvoice.Adapters.Oracle.Common.Extensions
+{
+    public static class DapperOracleExtension
+    {
+        public static async Task<T> QueryWrapperAsync<T>(string strConnection, Func<OracleConnection, Task<T>> handle)
+        {
+            using var connection = new OracleConnection(strConnection);
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+
+                return await handle(connection);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        public static async Task<int> ExecuteWrapperAsync(string strConnection, Func<OracleConnection, Task<int>> handle)
+        {
+            using var connection = new OracleConnection(strConnection);
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+
+                return await handle(connection);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
