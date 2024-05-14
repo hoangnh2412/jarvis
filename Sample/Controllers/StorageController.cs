011b2408d1c534b21cd8a4113d83c3464d3d21d9
Hoang Nguyen <hoangnh2412@gmail.com>
2024-05-14 17:50:23 +0700

    Fix bug

--- Sample/Controllers/StorageController.cs
+++ Sample/Controllers/StorageController.cs
@@ -1,6 +1,4 @@
-using Jarvis.Application.Interfaces;
 using Jarvis.Application.Interfaces.Repositories;
-using Jarvis.Persistence.DataContexts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sample.DataStorage;
@@ -18,89 +16,28 @@ public class StorageController : ControllerBase
         _serviceProvider = serviceProvider;
     }
 
-    [HttpGet("single")]
-    public async Task<IActionResult> SingleConnectionAsync(
-        [FromServices] ITenantUnitOfWork uow
+    [HttpGet("read")]
+    public async Task<IActionResult> ReadAsync(
+        [FromServices] ISampleUnitOfWork uow
     )
-    {
-        var repo = uow.GetRepository<IRepository<Tenant>>();
-        var tenants = await repo.GetQuery().ToListAsync();
-        return Ok(tenants);
-    }
-
-    [HttpGet("multiple")]
-    public async Task<IActionResult> MultipleConnectionAsync(
-        [FromServices] ISampleUnitOfWork uow,
-        [FromServices] IWorkContext workContext
-    )
-    {
-        var users = await GetUsersInternal(uow);
-
-        var tasks = new List<Task>();
-        for (int i = 0; i < 5; i++)
-        {
-            tasks.Add(GetUsers());
-        }
-        await Task.WhenAll(tasks);
-
-        return Ok(users);
-    }
-
-    [HttpGet("switch")]
-    public async Task<IActionResult> SwitchConnectionAsync(
-        [FromServices] IServiceProvider serviceProvider
-    )
-    {
-        var result = new Dictionary<string, KeyValuePair<string, List<User>>>();
-
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            var uow = scope.ServiceProvider.GetService<ISampleUnitOfWork>();
-            var dbContext = uow.GetDbContext() as DbContext;
-
-            var conn = dbContext.Database.GetConnectionString();
-            var users = await GetUsersInternal(uow);
-
-            result.Add("BeforeChange", new KeyValuePair<string, List<User>>(conn, users));
-        }
-
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            var uow = scope.ServiceProvider.GetService<ISampleUnitOfWork>();
-            var dbContext = uow.GetDbContext<StorageConnectionStringResolver>("tenant2") as DbContext;
-            var conn = dbContext.Database.GetConnectionString();
-            var users = await GetUsersInternal(uow);
-
-            result.Add("Change", new KeyValuePair<string, List<User>>(conn, users));
-        }
-
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            var uow = scope.ServiceProvider.GetService<ISampleUnitOfWork>();
-            var dbContext = uow.GetDbContext() as DbContext;
-
-            var conn = dbContext.Database.GetConnectionString();
-            var users = await GetUsersInternal(uow);
-
-            result.Add("AfterChange", new KeyValuePair<string, List<User>>(conn, users));
-        }
-        return Ok(result);
-    }
-
-    private async Task<List<User>> GetUsers()
-    {
-        using (var scope = _serviceProvider.CreateScope())
-        {
-            var uow = scope.ServiceProvider.GetService<ISampleUnitOfWork>();
-            List<User> users = await GetUsersInternal(uow);
-            return users;
-        }
-    }
-
-    private static async Task<List<User>> GetUsersInternal(ISampleUnitOfWork uow)
     {
         var repo = uow.GetRepository<IRepository<User>>();
         var users = await repo.GetQuery().ToListAsync();
-        return users;
+        return Ok(users);
+    }
+
+    [HttpGet("create")]
+    public async Task<IActionResult> CreateAsync(
+        [FromServices] ISampleUnitOfWork uow
+    )
+    {
+        var repo = uow.GetRepository<IRepository<User>>();
+        var users = await repo.InsertAsync(new DataStorage.User
+        {
+            Name = "sample",
+            Age = 11
+        });
+        await uow.CommitAsync();
+        return Ok(users);
     }
 }
