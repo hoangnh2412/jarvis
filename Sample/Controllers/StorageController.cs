fc2057d30573842968f80381567db596655f7145
Hoang Nguyen <hoangnh2412@gmail.com>
2024-05-15 01:37:17 +0700

    Add sample code

--- Sample/Controllers/StorageController.cs
+++ Sample/Controllers/StorageController.cs
@@ -40,4 +40,33 @@ public class StorageController : ControllerBase
         await uow.CommitAsync();
         return Ok(users);
     }
+
+    [HttpGet("update")]
+    public async Task<IActionResult> UpdateAsync(
+        [FromServices] ISampleUnitOfWork uow
+    )
+    {
+        var repo = uow.GetRepository<IRepository<User>>();
+        var user = await repo.GetQuery().FirstOrDefaultAsync(x => x.Id == 1);
+
+        user.Name = "updated";
+        user.Age = 12;
+
+        await repo.UpdateAsync(user);
+        await uow.CommitAsync();
+        return Ok(user);
+    }
+
+    [HttpGet("delete")]
+    public async Task<IActionResult> DeleteAsync(
+        [FromServices] ISampleUnitOfWork uow
+    )
+    {
+        var repo = uow.GetRepository<IRepository<User>>();
+        var user = await repo.GetQuery().FirstOrDefaultAsync(x => x.Id == 2);
+
+        await repo.DeleteAsync(user);
+        await uow.CommitAsync();
+        return Ok(user);
+    }
 }
