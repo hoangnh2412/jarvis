243b56d0b15797f46f0ceb5667dbfbaf5e40783d
Hoang Nguyen <hoangnh2412@gmail.com>
2024-08-19 22:09:34 +0700

    Implement CryptographyHelper

--- Jarvis.Domain.Shared/Utility/CryptographyHelper.cs
+++ Jarvis.Domain.Shared/Utility/CryptographyHelper.cs
@@ -8,7 +8,7 @@ namespace Jarvis.Domain.Shared.Utility;
 /// <summary>
 /// Helper class provides functions for Hashing, Encrypt/Descrypt
 /// </summary>
-public static partial class CryptographyHelper
+public static class CryptographyHelper
 {
     /// <summary>
     /// Hash data from byte[]
@@ -16,12 +16,10 @@ public static partial class CryptographyHelper
     /// <param name="alg">Hash algorithm name. Ex: MD5, SHA1, SHA256, SHA384, SHA512</param>
     /// <param name="data">Data for hashing</param>
     /// <returns></returns>
-    public static byte[] GetHash(HashAlgorithmName alg, byte[] data)
-    {
 #pragma warning disable SYSLIB0045 // Type or member is obsolete
-        return HashAlgorithm.Create(alg.Name).ComputeHash(data);
+    public static byte[] GetHash(HashAlgorithmName alg, byte[] data) => HashAlgorithm.Create(alg.Name).ComputeHash(data);
 #pragma warning restore SYSLIB0045 // Type or member is obsolete
-    }
+
 
     /// <summary>
     /// Hash data from string
@@ -29,8 +27,9 @@ public static partial class CryptographyHelper
     /// <param name="alg">Hash algorithm name. Ex: MD5, SHA1, SHA256, SHA384, SHA512</param>
     /// <param name="data">Data for hashing</param>
     /// <returns></returns>
-    public static byte[] GetHash(HashAlgorithmName alg, string data)
-    {
-        return GetHash(alg, Encoding.UTF8.GetBytes(data));
-    }
+    public static byte[] GetHash(HashAlgorithmName alg, string data) => GetHash(alg, Encoding.UTF8.GetBytes(data));
+
+    public static byte[] HMACSHA256(string key, string message) => HMACSHA256(Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(message));
+
+    public static byte[] HMACSHA256(byte[] key, byte[] message) => new HMACSHA256(key).ComputeHash(message);
 }
