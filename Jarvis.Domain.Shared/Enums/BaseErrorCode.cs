a22cd982e9f55c90306a1d2797075ec6ffc6284d
Hoang Nguyen <hoangnh2412@gmail.com>
2025-01-13 14:43:11 +0700

    Update version

--- Jarvis.Domain.Shared/Enums/BaseErrorCode.cs
+++ Jarvis.Domain.Shared/Enums/BaseErrorCode.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using Jarvis.Domain.Shared.ExceptionHandling;
 
 namespace Jarvis.Domain.Shared.Enums;
 
 /// <summary>
 /// The class defining the default error codes
 /// </summary>
-public class BaseErrorCode
+public class BaseErrorCode : IErrorCode
 {
     [Display(Description = "OK")]
     public const string OK = "00000";
